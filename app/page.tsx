d419e8e74a1f7258928af6c84ef5d176f3944939
Sebastian Sdorra <s.sdorra@gmail.com>
2024-09-04 14:38:25 +0200

    Fix some visual bugs

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,14 @@ export default function Home() {
       <ul className="mt-4 max-w-4xl space-y-4">
         {allPosts.map((post) => (
           <li key={post._meta.path}>
-            <article className="border-2 p-2 rounded-lg shadow-md hover:bg-teal-50 hover:border-teal-500">
+            <article className="border-2 p-4 rounded-lg shadow-md hover:bg-teal-50 hover:border-teal-500">
               <Link href={`/posts/${post._meta.path}`}>
+              <header className="flex justify-between items-c mb-2">
                 <h3 className="text-xl font-semibold">{post.title}</h3>
+                <p className="text-sm text-gray-600">{post.date}</p>
+                </header>
                 <p>{post.summary}</p>
+
               </Link>
             </article>
           </li>
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { allPosts } from "content-collections";
 import { notFound } from "next/navigation";
 import { MDXContent } from "@content-collections/mdx/react";
+import { Metadata } from "next";
 
 type Props = {
   params: {
@@ -15,17 +16,29 @@ export default function Post({ params: { slug } }: Props) {
   }
 
   return (
-    <article className="space-y-4">
+    <article className="space-y-4 max-w-[65ch]">
       <header>
         <h2 className="text-3xl font-semibold">{post.title}</h2>
       </header>
-      <div className="mt-4 prose">
+      <div className="mt-4 prose max-w-full">
         <MDXContent code={post.code} />
       </div>
-      <footer className="flex justify-between">
+      <footer className="flex justify-between items-center">
         <p className="text-sm text-gray-500">By {post.author}</p>
         <p className="mt-4">{post.date}</p>
       </footer>
     </article>
   );
+}
+
+export function generateMetadata({ params: { slug } }: Props): Metadata {
+  const post = allPosts.find((post) => post._meta.path === slug);
+  if (!post) {
+    return notFound();
+  }
+
+  return {
+    title: post.title,
+    description: post.summary,
+  };
 }
